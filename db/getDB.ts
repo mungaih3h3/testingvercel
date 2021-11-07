b4fb4f9e8d2ed2203c0bcad0a774f34de956dfc8
Mungai <itsmungai@gmail.com>
2021-11-07 06:07:52 +0300

    removed env

--- db/getDB.ts
+++ db/getDB.ts
@@ -1,10 +1,15 @@
 import { MongoClient } from "mongodb";
 
+//aws amplify doesn't pick up env variables soo here goes (repo's private anyway)
+const DB = {
+  url: "mongodb+srv://mungai:Ihc4rIhc4r@askfpl.cdjgt.mongodb.net/todos?retryWrites=true&w=majority",
+  name: "todos",
+};
+console.log(process.env);
+
 export async function getDB() {
-  const DB_URL = process.env.DB_URL;
-  if (!DB_URL) throw new Error("No db url");
-  const _db = await MongoClient.connect(DB_URL);
-  const DB_NAME = process.env.DB_NAME;
-  if (!DB_NAME) throw new Error("No db name");
-  return _db.db(DB_NAME);
+  if (!DB.url) throw new Error("No db url");
+  const _db = await MongoClient.connect(DB.url);
+  if (!DB.name) throw new Error("No db name");
+  return _db.db(DB.name);
 }
